1996cc898e01b109ee21e6be9b76ae1511a354b6
Lukas Sägesser <lukas.saegesser@scydev.ch>
2020-10-28 18:29:08 +0100

    - generate random lorem ipsum title and desc - generate random price

--- services/ContentGenerator.js
+++ services/ContentGenerator.js
@@ -4,6 +4,8 @@ const utils  = require('./utils/content');
 const _ = require('lodash');
 const uuid = require('uuid-random');
 
+const loremIpsum = require("lorem-ipsum").loremIpsum;
+
 /**
  * ContentGenerator.js service
  *
@@ -17,13 +19,13 @@ module.exports = {
       if (kind === 'collectionType' && Array.isArray(source)) {
         if (source.length > 0) {
           //console.log(source[i]);
-          let multiplier = 1000000;
+          let multiplier = 20;
           for (let k = 0; k < multiplier; k++) {
-            //source[0].id = i*multiplier + k; // make id unique by spacing it far apart
-            //source[0].slug = source[0].id;
-
             source[0].id = null; //uuid(); // null generates new id for new entry
-            source[0].slug = uuid();
+            source[0].slug = uuid(); //Date.now()
+            source[0].title = loremIpsum({count: 5, units: "words"}).toUpperCase();
+            source[0].description = loremIpsum({count: 30, units: "words"});
+            source[0].price = Math.floor(Math.random() * Math.floor(2000.00)) + (Math.floor(Math.random() * Math.floor(99.00)) / 100.0);
             //console.log(source[i]);
             await utils.generateItemByContentType(targetModel, source[0])
           }
